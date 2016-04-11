daa2754de3b788bfb52d2a4da0edf6328a30eff9
Shirshanka Das <sdas@linkedin.com>
2016-04-10 19:58:03 -0700

    Adding a placeholder job driver class, no implementation

---
+++ gobblin-cluster/src/main/java/gobblin/cluster/SimpleJobDriver.java
@@ -0,0 +1,13 @@
+package gobblin.cluster;
+
+/**
+ * A job driver that submits jobs to run in the cluster
+ *
+ */
+public class SimpleJobDriver {
+
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+  }
+}
